9c2ee20048a2250303b3f4e77da182aead97a4cf
vishnupriyan <priyanvishnu15@gmail.com>
2025-07-20 20:12:11 +0530

    creates constants for status

--- app/services.py
+++ app/services.py
@@ -1,6 +1,7 @@
 from django.db import transaction
 from rest_framework.exceptions import ValidationError
 from .models import Task, Resource
+from .constants import *
 
 def assign_resource_to_task(task_id, resource_id):
     with transaction.atomic():
@@ -12,7 +13,7 @@ def assign_resource_to_task(task_id, resource_id):
         except Resource.DoesNotExist:
             raise ValidationError("Resource not Found")
 
-        if resource.current_status != 'avalible':
+        if resource.current_status != RESOURCE_STATUS_AVAILABLE:
             raise ValidationError("Resource is not available to assign the Task.")
 
         if not resource.skills.filter(id=task.skill.id).exists():
@@ -21,7 +22,7 @@ def assign_resource_to_task(task_id, resource_id):
         task.assigned_resource = resource
         task.save()
 
-        resource.current_status = 'busy'
+        resource.current_status = RESOURCE_STATUS_BUSY
         resource.save()
 
         return task
